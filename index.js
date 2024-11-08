07cba874ece5113e0dc13148e76de6fd0b753223
Richard <rlew0925@gmail.com>
2024-11-08 00:46:57 -0700

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    const headquarters = 42;
+    return Math.abs(block - headquarters);
+}
+
+function distanceFromHqInFeet(block) {    
+    const feetPerBlock = 264;
+    const distanceInBlocks = distanceFromHqInBlocks(block);
+    return feetPerBlock * distanceInBlocks;
+}
+
+function distanceTravelledInFeet(start, destination) {    
+    return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice(start, destination) {    
+    const distanceTravelled = distanceTravelledInFeet(start, destination);
+    let rideFare;
+    if (distanceTravelled <= 400) {
+        rideFare = 0;
+    } else if (distanceTravelled > 400 && distanceTravelled <= 2000) {
+        rideFare = (distanceTravelled - 400) * 0.02;
+    } else if (distanceTravelled > 2000 && distanceTravelled <= 2500) {
+        rideFare = 25;
+    } else {
+        rideFare = 'cannot travel that far';
+    }
+    return rideFare;
+}
